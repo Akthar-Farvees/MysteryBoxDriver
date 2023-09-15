3ccb46100127f5ee9c0f4da83ba73bdbf44d04cc
Akthar farvees <junaideenakthar123@gmail.com>
2023-09-15 14:52:03 +0530

    check

--- src/Navigation/index.js
+++ src/Navigation/index.js
@@ -5,6 +5,7 @@ import SplashScreen from '../Screens/SplashScreen'
 import { StyleSheet, Text, View } from 'react-native';
 
 
+
 import React from 'react';
 
 const Stack = createNativeStackNavigator();
